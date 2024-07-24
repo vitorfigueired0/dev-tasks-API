2d43c4ebf2ff66ef8baf0c676794ea40f2a056ed
Vitor figueiredo <vittor.figueiredo17@gmail.com>
2024-07-24 16:22:01 -0300

    Configuração do Pino para gerar LOGS

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(Logger))
 
-  const config = new DocumentBuilder()
-  .setTitle('Dev Tasks API')
-  .setVersion('1.0.0')
-  .addTag('User')
-  .addTag('Task')
-  .addTag('Auth')
-  .build();
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Dev Tasks API')
+    .setVersion('1.0.0')
+    .addTag('User')
+    .addTag('Task')
+    .addTag('Auth')
+    .build();
 
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
-
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
   await app.listen(4000);
 }
 
