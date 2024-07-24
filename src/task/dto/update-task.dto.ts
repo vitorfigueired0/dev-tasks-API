16ff98e12de24fe23446fd735c8b0d72796125ac
Vitor figueiredo <vittor.figueiredo17@gmail.com>
2024-07-24 13:55:26 -0300

    Mapeamento dos atributos do objeto Task

--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateTaskDto } from './create-task.dto';
+import { TaskType } from '@prisma/client';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+ 
+}
