7d0e5641cbcfa087f6692fd79b4e8a629b9a4538
Vitor figueiredo <vittor.figueiredo17@gmail.com>
2024-07-24 13:17:55 -0300

    Documentando as propriedades dos DTO's de usuário

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,39 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateUserDto {
+
+  @ApiProperty({
+    description: 'The email will be used for login, paired with the password.',
+    example: 'vitor.figueiredo@mail.com',
+    required: true,
+    type: 'string'
+  })
+  email: string;
+
+  @ApiProperty({
+    description: 'The first name will be displayed wherever there is user information.',
+    example: 'Vitor',
+    required: true,
+    type: 'string'
+  })
+  firstName: string;
+
+  @ApiProperty({
+    description: `The last name will be concatenated with the user first name.`,
+    example: 'Figueiredo',
+    required: true,
+    type: 'string'
+  })
+  lastName: string;
+
+  @ApiProperty({
+    description: `
+      The password will be used for login if the user has not signed up via Google and must be between 
+      6-32 characters, including uppercase, lowercase, numbers, and a special character`,
+    example: 'Teste@1234',
+    required: true,
+    type: 'string'
+  })
+  password: string;
+
+}
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,39 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+   
+  @ApiProperty({
+    description: 'The email will be used for login, paired with the password.',
+    example: 'vitor.figueiredo@mail.com',
+    required: false,
+    type: 'string'
+  })
+  email: string;
+
+  @ApiProperty({
+    description: 'The first name will be displayed wherever there is user information.',
+    example: 'Vitor',
+    required: false,
+    type: 'string'
+  })
+  firstName: string;
+
+  @ApiProperty({
+    description: `The last name will be concatenated with the user first name.`,
+    example: 'Figueiredo',
+    required: false,
+    type: 'string'
+  })
+  lastName: string;
+
+  @ApiProperty({
+    description: `
+      The password will be used for login if the user has not signed up via Google and must be between 
+      6-32 characters, including uppercase, lowercase, numbers, and a special character`,
+    example: 'Teste@1234',
+    required: false,
+    type: 'string'
+  })
+  password: string;
+}
